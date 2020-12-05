94f2cb7864a90a30ffc82f29acb74e16ad2aba85
amramer45 <69049940+amramer45@users.noreply.github.com>
2020-12-05 12:20:29 -0500

    updates

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,78 @@
-'PyBank
-'Your task is to create a Python script that analyzes the records to calculate each of the following:
-'The total number of months included in the dataset
-'The net total amount of "Profit/Losses" over the entire period
-'Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-'The greatest increase in profits (date and amount) over the entire period
-'The greatest decrease in losses (date and amount) over the entire period
-'In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+#PyBank
+#Your task is to create a Python script that analyzes the records to calculate each of the following:
+#The total number of months included in the dataset
+#The net total amount of "Profit/Losses" over the entire period
+#Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+#The greatest increase in profits (date and amount) over the entire period
+#The greatest decrease in losses (date and amount) over the entire period
+#In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+
+
+
+
+#Import necessary dependencies 
+import os
+import csv
+
+#Path to collect data from the Resources folder
+pybank_csv = os.path.join('OneDrive', 'Desktop', 'python-challenge', 'PyBank',  'Resources', 'pybank.csv')
+
+
+data = []
+with open(pybank_csv, 'r') as csvfile:
+    #Split the data on comas
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    header = next(csvreader)
+
+    #Loop through the data
+    for row in csvreader:
+        date = row[0]
+        profit = int(row[1])
+        data.append([date, profit])
+
+
+# Calculate total months
+total_month = total_months + 1
+
+#Calculate the net total
+net_total = net_total + int(row[1])
+
+#Calculate the net total
+net_total = net_total + int(row[1])
+
+#Calculate change in profit
+profit_change = int(row[1]) - previous_value
+
+checker = {}
+total = 0
+for row in data:
+    date = row[0]
+    profit = row[1]
+    total = total + profit
+
+    if date not in checker:
+        checker[date] = True
+ print(len(checker.keys()))
+ print(total)
+
+changes = []
+for i in range(len(data) - 1):
+    changes.append(data[i][1] - data[i + 1][1])
+print(sum(changes)/len(changes))
+
+#set format for printing
+output = (
+    f"Financial Analysis \n"
+    f"------------------ \n"
+    f"Total Months: {total_votes} \n"
+    f"Average Change: {average_change} \n"
+    f"Greatest Increase in Profits: {greatest_increase} \n"
+    f"Greatest Decrease in Profits: {greatest_decrease}
+)
+
+#Export text file
+print(output)
+with open("analysis/output.txt")
+    txt_file
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,8 +1,77 @@
-'PyPoll
-'Python script that analyzes the votes and calculates each of the following:
-'The total number of votes cast
-'A complete list of candidates who received votes
-'The percentage of votes each candidate won
-'The total number of votes each candidate won
-'The winner of the election based on popular vote.
-'In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+#The total number of votes cast
+#A complete list of candidates who received votes
+#The percentage of votes each candidate won
+#The total number of votes each candidate won
+#The winner of the election based on popular vote.
+#In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+
+import os
+import csv
+import operator
+from collections import defaultdict
+
+#Path to csv data
+pypoll_csv = os.path.join('OneDrive', 'Desktop', 'python-challenge', 'PyPoll', 'Resources', 'pypoll.csv')
+
+#total_votes = 0
+candidate_votes = {}
+candidate_votes_percentage = {}
+combined_votes_percentage = defaultdict(list)
+candidates = []
+percentage = 0
+winner_name = []
+
+#Read csv file
+with open(pypoll_csv) as csvfile:
+
+    #Split the data on the commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+    
+    header = next(csvreader)
+
+    #Loop through the data
+    for row in csvreader:
+
+        #Calculate total votes cast
+        total_votes = total_votes + 1
+
+        name = row[2]
+
+        #Calculate the total votes each candidate won
+        if name not in candidate_votes:
+            candidate_votes[name] = 1
+        else:
+            if candidate_votes[name] == 1:
+                candidates.append(name)
+                candidate_votes[name] += 1
+            else:
+                candidate_votes[name] += 1
+
+#Calculate the percentage of votes each candidate won and 
+candidate_votes_percentage = dict(candidate_votes)
+
+vote_res = " "
+for key, value in candidate_votes_percentage.items():
+    vote_percentage = float(value)/float(total_votes) * 100
+    vote_res += key + ' ' + f'{vote_percentage:.3f}% ({value})\n'
+
+
+#Determine winner of the election 
+winner_name = max(candidate_votes.items(), key=operator.itemgetter(1))[0]
+
+#set format for printing
+output = (
+    f"Election Results \n"
+    f"------------------ \n"
+    f"Total Votes: {total_votes} \n"
+    f"------------------ \n"
+    f"{vote_res} \n"
+    f"------------------ \n"
+    f"Winner {winner_name} \n"
+    f"------------------ \n"
+)
+
+#Output
+print(output)
+with open("analysis/output.txt")
+    txt_file'PyPoll
