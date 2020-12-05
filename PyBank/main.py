cb1675675451f72f2b7075f2652e52f9b3affa3a
amramer45 <69049940+amramer45@users.noreply.github.com>
2020-12-05 15:08:49 -0500

    updates

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,3 @@
-#PyBank
-#Your task is to create a Python script that analyzes the records to calculate each of the following:
 #The total number of months included in the dataset
 #The net total amount of "Profit/Losses" over the entire period
 #Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
@@ -7,59 +5,42 @@
 #The greatest decrease in losses (date and amount) over the entire period
 #In addition, your final script should both print the analysis to the terminal and export a text file with the results.
 
-
-
-#Import necessary dependencies 
+#Import dependencies 
 import os
 import csv
 
 #Path to collect data from the Resources folder
 pybank_csv = os.path.join('Resources', 'pybank.csv')
 
-net_total = 0
-total_months = 0
+total_months = []
+net_total = []
+average_change = []
 
-data = []
-with open(pybank_csv, 'r') as csvfile:
+#Read csv
+with open(pybank_csv) as csvfile:
     
     #Split the data on comas
     csvreader = csv.reader(csvfile, delimiter=',')
 
+    #Skip the header
     header = next(csvreader)
 
-    #Loop through the data
-    for row in csvreader:
-        date = row[0]
-        net_total = net_total + int(row[1])
-        data.append([date, net_total])
+#Loop through the data
+for row in csvreader:
 
+    total_months.append(row[0])
+    net_total.append(int(row[1]))
 
-# Calculate total months
-total_months = total_months + 1
+#Change in profits/losses
+for i in range(len(net_total)-1):
 
-#Calculate change in profit
-profit_change = int(row[1]) - 1
+    #Average change in profits/losses
+    average_change.append(net_total[i + 1] - net_total[i])
 
-checker = {}
-total = 0
-for row in data:
-    date = row[0]
-    profit = row[1]
-    total = total + profit
-
-    if date not in checker:
-        checker[date] = True
-
-changes = []
-for i in range(len(data) - 1):
-    changes.append(data[i][1] - data[i + 1][1])
-
-    average_change = sum(changes)/len(changes)
-
-#Find the greatest increase in profits/losses
+#Greatest increase in profits/losses
 greatest_increase = max(average_change)
 
-#Find the greatest decrease in profits/losses
+#Greatest decrease in profits/losses
 greatest_decrease = min(average_change)
 
 #set format for printing
@@ -76,4 +57,3 @@ output = (
 print(output)
 with open("analysis/output.txt", "w") as txt_file:
     txt_file.write(output)
-
